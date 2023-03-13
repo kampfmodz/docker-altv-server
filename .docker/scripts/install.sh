188aa335c6ab70c6f62b15d1c15c9eda46df4506
kampfmodz <43520105+kampfmodz@users.noreply.github.com>
2023-03-13 21:26:02 +0100

    fix 2

--- .docker/scripts/install.sh
+++ .docker/scripts/install.sh
@@ -13,7 +13,7 @@ INSTALL_CSHARP_MODULE=${INSTALL_CSHARP_MODULE:-"true"}
 LIBNODE_VERSION=${LIBNODE_VERSION:-"102"}
 
 apt-get update
-apt-get install -y wget gnupg libatomic1 libc-bin apt-transport-https
+apt-get install -y wget gnupg libatomic1 libc-bin apt-transport-https libnode72
 
 # setup default altv-server files
 mkdir -p /opt/altv/modules /opt/altv/resources /opt/altv/data
@@ -28,6 +28,7 @@ chmod +x /opt/altv/altv-server /root/entrypoint.sh
 
 # setup optional js module
 if [ "$INSTALL_JS_MODULE" = "true" ]; then
+    apt-get install libnode72 -y
     mkdir -p /opt/altv/modules/js-module/
     wget --no-cache -q -O /opt/altv/modules/js-module/libnode.so.${LIBNODE_VERSION} https://cdn.altv.mp/js-module/${BRANCH}/x64_linux/modules/js-module/libnode.so.${LIBNODE_VERSION}
     wget --no-cache -q -O /opt/altv/modules/js-module/libjs-module.so https://cdn.altv.mp/js-module/${BRANCH}/x64_linux/modules/js-module/libjs-module.so
@@ -41,7 +42,6 @@ if [ "$INSTALL_CSHARP_MODULE" = "true" ]; then
     rm -f packages-microsoft-prod.deb
     apt-get update
     apt-get install dotnet-runtime-7.0 -y
-    apt-get install libnode72 -y
     # install altV module
     wget --no-cache -q -O /opt/altv/modules/libcsharp-module.so https://cdn.altv.mp/coreclr-module/${BRANCH}/x64_linux/modules/libcsharp-module.so
     mkdir -p /usr/share/dotnet/host/fxr/
